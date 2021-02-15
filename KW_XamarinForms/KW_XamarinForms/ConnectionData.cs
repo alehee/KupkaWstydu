a4e4c705887023a88aeb69ba95c9f871f6dc6d5e
Aleksander Heese <36664250+alehee@users.noreply.github.com>
2021-02-15 14:47:50 +0100

    Security Update

--- KW_XamarinForms/KW_XamarinForms/ConnectionData.cs
+++ KW_XamarinForms/KW_XamarinForms/ConnectionData.cs
@@ -6,6 +6,6 @@ namespace KW_XamarinForms
 {
     class ConnectionData
     {
-        public const string CONNECTION_STRING = "server=yourIp;port=3306;user=yourUser;password=yourPassword;database=yourDatabase;";
+        public const string CONNECTION_STRING = "server=aleksanderheese.pl;port=3306;user=u986763087_kwadmin;password=KwP@ssw0rd;database=u986763087_kupkawstydu;";
     }
 }
